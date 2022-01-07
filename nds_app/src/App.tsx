6b7d8fe537fc4d4525757396bf658a99d3a6f13d
Mikhail Ashcheulov <ashcheulov.mr@gmail.com>
2022-01-08 00:25:28 +0300

    some experiments with react

--- nds_app/src/App.tsx
+++ nds_app/src/App.tsx
@@ -1,8 +1,46 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {useKeycloak} from "@react-keycloak/web";
+
+interface IData {
+    name: string
+    surname: string
+}
 
 const App: FC = () => {
     const {keycloak} = useKeycloak()
+
+    const [data, setData] = useState<IData>()
+
+    const req = async () => {
+        const url = "http://api.aquarel.ru" + "/api/demo"
+        // const token = keycloak.token
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${keycloak.token}`
+                },
+                // new Headers({
+                //     'Authorization': `Bearer ${keycloak.token}`
+                // })
+            });
+            const json = await response.json();
+            console.log(json);
+            setData(json)
+            console.log(keycloak.idTokenParsed);
+        } catch
+            (error) {
+            console.log("error", error);
+        }
+    };
+    useEffect(() => {
+        if (keycloak.token !== undefined)
+            req()
+    },[keycloak.token])
+
+
     return (
         <div className="App">
             {!keycloak.authenticated && (
@@ -21,13 +59,14 @@ const App: FC = () => {
                 >
                     Logout (
                     {
-                        // @ts-ignore
-                        keycloak.tokenParsed.given_name
+                        data?.surname + " " + data?.name
+                        // keycloak.token
                     } {
 
                 })
                 </button>
             )}
+
         </div>
     );
 }
